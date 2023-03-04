e75a5009ea9994b4efb2397e3772675d2c98ad24
Blink Tenor <blinktenor@gmail.com>
2023-03-04 10:43:22 -0600

    cleanup code

--- components/QuestionTimer.js
+++ components/QuestionTimer.js
@@ -2,16 +2,11 @@ import { useEffect } from 'react';
 
 const QuestionTimer = (props) => {
   let intervalId = 0;
-  const timerUp = () => {
-    console.log('timerUp ' + intervalId);
-  };
 
-  let intervalid;
   useEffect(() => {
-    intervalId = setInterval(() => {console.log("Iterate " + intervalId); }, 5000);
+    intervalId = setInterval(props.callback, 5000);
 
       return function cleanup() {
-        console.log("cleaning up");
         clearInterval(intervalId);
       };
   }, []);
